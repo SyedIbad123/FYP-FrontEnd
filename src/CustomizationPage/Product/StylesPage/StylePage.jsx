fba3546c5bbbd9189a6025356a0d8a2a5bde1813
mahmed74 <ahmedsheikh0704@gmail.com>
2024-05-05 15:20:51 +0500

    api integration

--- src/CustomizationPage/Product/StylesPage/StylePage.jsx
+++ src/CustomizationPage/Product/StylesPage/StylePage.jsx
@@ -1,26 +1,41 @@
 // import bookmarkImage from "../images/bookmark1.jpeg";
 
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 const StylePage = () => {
 	const [images, setImages] = useState([]);
 
-	// useEffect(() => {
-	// 	const fetchImages = async () => {
-	// 		const response = await fetch("bd.json");
-	// 		const imageData = await response.json();
-	// 		console.log("image data",imageData);
-	// 		setImages(imageData);
-	// 	};
+	useEffect(() => {
+		// const fetchImages = async () => {
+		// 	const response = await fetch("bd.json");
+		// 	const imageData = await response.json();
+		// 	console.log("image data",imageData);
+		// 	setImages(imageData);
+		// };
 
-	// 	fetchImages();
-	// }, []);
+		// fetchImages();
+		console.log("USE EFFECT =====")
+		const url = 'http://localhost:8000/api/v2/image/get-all-images';
+		// Making the GET request using Axios
+		axios.get(url)
+		.then(response => {
+			console.log('Response: IMAGES', response.data);
+			setImages(response?.data?.images)
+			// Handle response data
+		})
+		.catch(error => {
+			console.error('Error:', error);
+			// Handle error
+		});
+	}, []);
+	
 
 	return (
 		<div>
 			<h2>Your Cart</h2>
 			{images.map((image) => (
-				<img key={image.link} src={image.link} alt={image.filename} />
+				<img key={image?.imageUrl} src={image.imageUrl} alt={image?.filename} />
 			))}
 		</div>
 	);
